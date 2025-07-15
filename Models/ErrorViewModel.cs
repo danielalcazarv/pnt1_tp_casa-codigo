8ff0ad0afe458a04a32c1cf8b49a831ba50010c4
Usuario <danielalcazar54@gmail.com>
2025-07-15 15:49:06 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace casa_codigo_cursos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace casa_codigo_cursos.Models
+{
+    public class Usuario
+    {
+
+    }
+}
+
+
